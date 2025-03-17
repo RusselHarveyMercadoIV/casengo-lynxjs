3428aada0d2936fc54020177fd3f4962e319a8c6
RusselHarveyMercadoIV <russelmercadosacs@gmail.com>
2025-03-17 20:53:10 +0800

    add custom button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import styles from './Button.module.css';
+
+type ButtonType = {
+  text: string;
+  variant: 'orange' | 'white' | 'plain';
+};
+
+export default function Button({ text, variant }: ButtonType) {
+  let buttonVariantCss = styles.plain;
+  if (variant === 'orange') buttonVariantCss = styles.orange;
+  if (variant === 'white') buttonVariantCss = styles.white;
+
+  return (
+    <view
+      className={`flex justify-center items-center border border-4 rounded-2xl min-w-[350px] min-h-[60px]  ${buttonVariantCss}`}
+    >
+      <text className="text-xl text-white font-bold">{text}</text>
+    </view>
+  );
+}
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,5 +1,66 @@
-import styles from './styles/Start.module.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+import { DIAGNOSTIC_QUESTIONS } from '../constants/question-bank.js';
+import type { AcademicStatus, profilingDataType } from '../types/types.js';
+
+import OrangeButton from '../assets/images/button-orange-1.png';
+import WhiteButton from '../assets/images/button-white-1.png';
+import Button from '../components/Button/Button.jsx';
+
+const profilingData: profilingDataType = {
+  countryExam: '',
+  appNotice: '',
+  academicStatus: '',
+  goal: '',
+  comittment: '',
+  start: '',
+};
 
 export default function Start() {
-  return <view className="container"></view>;
+  const [formData, setFormData] = useState<profilingDataType>(profilingData);
+  const [currentStep, setCurrentStep] = useState<number>(0);
+
+  const navigation = useNavigate();
+
+  const handleStepChange = (step: number) => {
+    if (currentStep === 6 && step > 0) {
+      navigation('quiz', {
+        state: {
+          questions: DIAGNOSTIC_QUESTIONS,
+          academicStatus: formData?.academicStatus as AcademicStatus,
+        },
+      });
+    } else {
+      setCurrentStep((prevStep) => prevStep + step);
+    }
+  };
+
+  let content = (
+    <view className="flex-col flex gap-14 mb-14">
+      <view className="flex-col relative flex gap-10 justify-center items-center">
+        <text className="text-2xl text-[#323842]">
+          Already have an account?
+        </text>
+        <Button text={'SIGN IN'} variant="orange" />
+      </view>
+      <view className="border border-1 border-[#dee1e6]" />
+      <view className="flex-col relative flex gap-10 justify-center items-center">
+        <text className="text-2xl text-[#323842] line-30">New to Casengo?</text>
+        <image src={WhiteButton} className=" w-[350px] h-[65px]" />
+        <view
+          className=" w-[350px] h-[65px] absolute top-[60px] flex justify-center items-center"
+          bindtap={() => handleStepChange(1)}
+        >
+          <text className="text-xl text-[#ed7d2d] font-bold">GET STARTED</text>
+        </view>
+      </view>
+    </view>
+  );
+
+  return (
+    <view className="container justify-center items-center h-full">
+      {content}
+    </view>
+  );
 }
