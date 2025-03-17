e180f0cd15d030210b6a436f80e4315fc2ac0aa3
RusselHarveyMercadoIV <russelmercadosacs@gmail.com>
2025-03-17 21:47:12 +0800

    refactor the step to a component

---
+++ src/components/Steps.tsx
@@ -0,0 +1,79 @@
+import Button from './Button/Button.jsx';
+
+export default function Steps({
+  currentStep,
+  icon,
+  onStepChange,
+  formData,
+  header,
+}: {
+  currentStep: number;
+  icon?: string;
+  onStepChange: () => void;
+  formData: any;
+  header?: string;
+}) {
+  const stepsForm = 'w-6 h-6 rounded-full';
+  const finishedStep = 'bg-[#ed7d2d]';
+  const unfinishedStep = 'bg-[#dee1e6]';
+
+  return (
+    <>
+      <Button
+        variant="plain"
+        icon={icon && <image src={icon} className="w-6 h-6" />}
+        onTap={onStepChange}
+        className="absolute top-6 left-6 border-none"
+      />
+      <view className="flex flex-row items-center justify-center w-[350px]">
+        <view
+          className={
+            ' flex flex-row justify-center items-center gap-4 bg-[#f8f9fa] w-[230px] h-[40px] rounded-full'
+          }
+        >
+          <view
+            className={`${stepsForm} ${
+              currentStep >= 1 ? finishedStep : unfinishedStep
+            }`}
+          />
+          <view
+            className={`${stepsForm} ${
+              currentStep >= 2 || formData.appNotice !== ''
+                ? finishedStep
+                : unfinishedStep
+            }`}
+          />
+          <view
+            className={`${stepsForm} ${
+              currentStep >= 3 || formData.academicStatus !== ''
+                ? finishedStep
+                : unfinishedStep
+            }`}
+          />
+          <view
+            className={`${stepsForm} ${
+              currentStep >= 4 || formData.goal !== ''
+                ? finishedStep
+                : unfinishedStep
+            }`}
+          />
+          <view
+            className={`${stepsForm} ${
+              currentStep >= 5 || formData.comittment !== ''
+                ? finishedStep
+                : unfinishedStep
+            }`}
+          />
+          <view
+            className={`${stepsForm} ${
+              currentStep === 6 || formData.start !== ''
+                ? finishedStep
+                : unfinishedStep
+            }`}
+          />
+        </view>
+      </view>
+      {header && <text className="text-2xl w-[350px] mt-10  ">{header}</text>}
+    </>
+  );
+}
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -8,6 +8,7 @@ import Button from '../components/Button/Button.jsx';
 import icons from '../constants/icons.js';
 import { headers } from '../constants/start.js';
 import Separator from '../components/Separator.jsx';
+import Steps from '../components/Steps.jsx';
 
 const profilingData: profilingDataType = {
   countryExam: '',
@@ -138,10 +139,6 @@ export default function Start() {
     );
   }
 
-  const stepsForm = 'w-6 h-6 rounded-full';
-  const finishedStep = 'bg-[#ed7d2d]';
-  const unfinishedStep = 'bg-[#dee1e6]';
-
   const steps = Object.keys(formData);
   const current = formData[steps[currentStep - 1] as keyof profilingDataType];
   const isContinue = current === '';
@@ -149,65 +146,12 @@ export default function Start() {
   return (
     <view className="container mt-[25px] flex-col justify-start items-center h-full">
       {currentStep > 0 && (
-        <>
-          <Button
-            variant="plain"
-            icon={<image src={icons.leftArrow} className="w-6 h-6" />}
-            onTap={() => handleStepChange(-1)}
-            className="absolute top-6 left-6 border-none"
-          />
-          <view className="flex flex-row items-center justify-center w-[350px]">
-            <view
-              className={
-                ' flex flex-row justify-center items-center gap-4 bg-[#f8f9fa] w-[230px] h-[40px] rounded-full'
-              }
-            >
-              <view
-                className={`${stepsForm} ${
-                  currentStep >= 1 ? finishedStep : unfinishedStep
-                }`}
-              />
-              <view
-                className={`${stepsForm} ${
-                  currentStep >= 2 || formData.appNotice !== ''
-                    ? finishedStep
-                    : unfinishedStep
-                }`}
-              />
-              <view
-                className={`${stepsForm} ${
-                  currentStep >= 3 || formData.academicStatus !== ''
-                    ? finishedStep
-                    : unfinishedStep
-                }`}
-              />
-              <view
-                className={`${stepsForm} ${
-                  currentStep >= 4 || formData.goal !== ''
-                    ? finishedStep
-                    : unfinishedStep
-                }`}
-              />
-              <view
-                className={`${stepsForm} ${
-                  currentStep >= 5 || formData.comittment !== ''
-                    ? finishedStep
-                    : unfinishedStep
-                }`}
-              />
-              <view
-                className={`${stepsForm} ${
-                  currentStep === 6 || formData.start !== ''
-                    ? finishedStep
-                    : unfinishedStep
-                }`}
-              />
-            </view>
-          </view>
-          <text className="text-2xl w-[350px] mt-10  ">
-            {headers[currentStep - 1]?.header}
-          </text>
-        </>
+        <Steps
+          currentStep={currentStep}
+          formData={formData}
+          icon={icons.leftArrow}
+          onStepChange={() => handleStepChange(-1)}
+        />
       )}
       {content}
       {currentStep > 0 && (
